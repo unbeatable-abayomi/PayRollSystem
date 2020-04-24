0416324d79a262c148d6e71219f01c1d99bfc8b2
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-24 16:12:21 +0100

    Added Invoice

--- PayRollSystem/Invoice.cs
+++ PayRollSystem/Invoice.cs
@@ -6,6 +6,62 @@ namespace PayRollSystem
 {
      public class Invoice:IPayment
      {
+        public string ItemName { get; set; }
+        public string ItemDesc { get; set; }
 
-     }
+        private decimal price;
+        private int quantity;
+
+        public Invoice(string item, string desc, decimal itemprice, int itemqty)
+        {
+            ItemName = item;
+            ItemDesc = desc;
+            Price = itemprice;
+            Quantity = itemqty;
+
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if(value > 0m)
+                {
+                   price = value;
+                }
+                else
+                {
+                    throw new System.ArgumentOutOfRangeException("Price Must be greater than zero");
+                }
+            }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                if(value > 0)
+                {
+                    quantity = value;
+                }
+                else
+                {
+                    throw new System.ArgumentOutOfRangeException("Quantity must be greater than zero");
+                }
+            }
+        }
+
+        public decimal GetPaymentAmount()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Invoice:\n {ItemName}\n Item Description: {ItemDesc}\n Quantity: {Quantity}\n Price {Price}\n";
+        }
+
+    }
 }
